ac40c0d32df0aff6e57ee24ae394528e1d0c2d99
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-01-13 02:39:55 +0530

    Fix: Align notification bell icon using flexbox in header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -484,7 +484,10 @@ function OEHeader(props) {
                           <div
                             style={{
                               position: "relative",
-                              display: "inline-block",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              height: "100%"
                             }}
                           >
                             {!notificationsOpen ? (
